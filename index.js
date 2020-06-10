21ccffa41fc5800a4d28dafb95249ee83ec4a753
abbu1999 <abhishekbisht559@gmail.cm>
2020-06-10 08:03:56 +0530

    projects

---
+++ index.js
@@ -0,0 +1,37 @@
+// var number1= 14;
+// var number2= 16;
+// console.log(number1+number2);
+
+
+// var marks={
+//     aman: 40.45,
+//     katte: 45,
+//     abbu: 42.15,
+// }
+// console.log(marks);
+// var number = 10;
+// var str = "this is string ";
+
+
+// var arr=[1,2,3,4,5];
+// console.log(arr);
+// for loop
+    // let  arr =[1,2,3,4,5,6,7];
+    //     for (let i=0;i<=arr.length;i++){
+    //     console.log(arr[i])
+    // }
+
+    // for each loop
+// var arr=[1,2,3,4,5,6];
+// arr.forEach(function(element){
+//     console.log(element);//in this we create a function named element and give that the all values of arr and print the values of arr...
+// })
+// while loop
+// let arr =[1,2,3,4,5];
+// let j=0;
+// while(j<arr.length)
+// {
+//     console.log(arr[j])
+//     j++ ;
+// }
+
